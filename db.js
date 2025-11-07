916565f6f10ba7e367f787866e072e2de75f3d2c
Mxhmmd <muhammadali.magashi@gmail.com>
2025-11-07 06:47:02 +0100

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'engihub_app',
   host: process.env.DB_HOST || 'dpg-d46la53ipnbc73eogj5g-a',
   database: process.env.DB_NAME || 'engihub',
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || 'Rm4KFpNIeYqqxveHHBuHbe4FYlth6qwB',
   port: process.env.DB_PORT || 5432,
 });
 
