c0683e6e855f4814302b3e9da5e1ecefba3e9426
Mxhmmd <muhammadali.magashi@gmail.com>
2025-11-07 06:16:22 +0100

    Update high_score.js

--- routes/high_score.js
+++ routes/high_score.js
@@ -1,12 +1,12 @@
 // routes/high_score.js
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../db'); // PostgreSQL pool
 
-// Get all high_score
+// GET all high scores
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM high_score');
+    const result = await pool.query('SELECT * FROM high_score'); // query correct table
     res.json(result.rows);
   } catch (err) {
     console.error(err);
@@ -15,4 +15,3 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
-
