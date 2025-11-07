4cc65cc64d1c82c1af2faa6d958494ac8b6e831f
Mxhmmd <muhammadali.magashi@gmail.com>
2025-11-07 05:52:50 +0100

    Update calculations.js

--- routes/calculations.js
+++ routes/calculations.js
@@ -1,4 +1,4 @@
-// routes/users.js
+// routes/calculations.js
 const express = require('express');
 const router = express.Router();
 const pool = require('../db'); // adjust if your pool is in another file
