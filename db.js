2bd846bbfeb29ef0f71c07f555862f05f17cd2fe
Mxhmmd <muhammadali.magashi@gmail.com>
2025-11-07 06:32:03 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'engihub_app',
+  host: process.env.DB_HOST || 'dpg-d46la53ipnbc73eogj5g-a',
   database: process.env.DB_NAME || 'engihub',
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
